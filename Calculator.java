7f63736eed0dd50fc9a1761d5dfd542838682608
McCannAaron <mccannaaront@gmail.com>
2023-09-29 21:14:29 -0400

    Finished calculator

--- Calculator.java
+++ Calculator.java
@@ -1,24 +1,25 @@
-import java.util.Scanner;
-import java.lang.Math;
-public class Calculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+import java.util.Scanner; // Import scanner
+import java.lang.Math; // Import Math library from package
 
-        double num1;
+public class Calculator { // Class that runs calculator
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Accepts inputs
+
+        double num1; // All variables needed
         double num2;
         double stored;
         double result;
         String operator;
 
-        num2 = 0.0;
+        num2 = 0.0; // Initialize numbers
         stored = 0.0;
 
         System.out.print("Please enter your first number: ");
-        num1 = scanner.nextDouble();
+        num1 = scanner.nextDouble(); // Read from user the number they input
 
         System.out.println();
 
-        System.out.println("Available options include: ");
+        System.out.println("Available options include: "); // Print list of available options
         System.out.println("Memory+: M+");
         System.out.println("Memory-: M-");
         System.out.println("Memory Store: MS");
@@ -34,77 +35,117 @@ public class Calculator {
 
         System.out.println();
 
-        System.out.print("Please enter one (M+, M-, MS, MR, %, /, *, +, -, SQRT, ^, Q): ");
-        operator = scanner.next().toUpperCase();
+        boolean quit = false;
 
-        if (!(operator.equals("M+")) && !(operator.equals("M-")) && !(operator.equals("MS")) && !(operator.equals("MR"))
-        && !(operator.equals("SQRT")) && !(operator.equals("Q"))) {
-            System.out.println();
-            System.out.print("Please enter your second number: ");
-            num2 = scanner.nextDouble();
-        }
+        do { // do while loop to ensure calculator runs until user wants to stop
+            System.out.print("Please enter one (M+, M-, MS, MR, %, /, *, +, -, SQRT, ^, Q): ");
+            operator = scanner.next().toUpperCase(); // Reads the operator from above
 
-        switch (operator) {
-            case "MS":
+            if (!(operator.equals("M+")) && !(operator.equals("M-")) && !(operator.equals("MS")) && !(operator.equals("MR"))
+                    && !(operator.equals("SQRT")) && !(operator.equals("Q"))) { // If statement to ensure certain operations use one number
                 System.out.println();
-                stored = num1;
-                System.out.println("The following is stored: " + stored);
-            case "%":
-                System.out.println();
-                System.out.print("Do you want multiply(*), divide(/), add(+), or subtract(-) the percent: ");
-                String operator2 = scanner.next();
-                if (operator2.equals("+")) {
-                    result = (num1 + ((num2 / 100) * num1));
-                    System.out.println("Result of addition with percent: " + result);
-                } else if (operator2.equals("-")) {
-                    result = (num1 - ((num2 / 100) * num1));
-                    System.out.println("Result of subtraction with percent: " + result);
-                } else if (operator2.equals("*")) {
-                    result = (num1 * (num2 / 100));
-                    System.out.println("Result of multiplication with percent: " + result);
-                } else if (operator2.equals("/")) {
-                    result = (num1 / (num2 / 100));
-                    System.out.println("Result of division with percent: " + result);
-                }
-                break;
-            case "/":
-                if (num2 != 0) {
+                System.out.print("Please enter your second number: ");
+                num2 = scanner.nextDouble(); // Reads second number if needed
+            }
+
+            switch (operator) { // Switch statement for options
+                case "MS": // Case for memory store
                     System.out.println();
-                    result = num1 / num2;
-                    System.out.println("Result of division: " + result);
-                } else {
-                    System.out.println("Cannot divide by zero. Please try again.");
-                }
-                break;
-            case "*":
-                System.out.println();
-                result = num1 * num2;
-                System.out.println("Result of multiplication: " + result);
-                break;
-            case "+":
-                System.out.println();
-                result = num1 + num2;
-                System.out.println("Result of addition: " + result);
-                break;
-            case "-":
-                System.out.println();
-                result = num1 - num2;
-                System.out.println("Result of subtraction: " + result);
-                break;
-            case "SQRT":
-                System.out.println();
-                result = Math.sqrt(num1);
-                System.out.println("Result of square root: " + result);
-                break;
-            case "^":
-                System.out.println();
-                result = Math.pow(num1, num2);
-                System.out.println("Result of exponent: " + result);
-                break;
-            case "Q":
-                System.out.println();
-                System.out.println("Thank you for using the calculator.");
-                break;
+                    stored = num1;
+                    System.out.println("The following number is stored: " + stored);
+                    System.out.println();
+                    break;
+                case "MR": // Case of memory recall
+                    System.out.println();
+                    System.out.println("The number you stored is: " + stored);
+                    System.out.println();
+                    break;
+                case "M+": // Case for memory +
+                    System.out.println();
+                    System.out.print("Please number you want to add to the stored value: ");
+                    num2 = scanner.nextDouble();
+                    result = stored + num2;
+                    System.out.println("Result of Memory+: " + result);
+                    System.out.println();
+                    break;
+                case "M-": // Case for memory -
+                    System.out.println();
+                    System.out.print("Please number you want to subtract from the stored value: ");
+                    num2 = scanner.nextDouble();
+                    result = stored + num2;
+                    System.out.println("Result of Memory-: " + result);
+                    System.out.println();
+                    break;
+                case "%": // Case for percent
+                    System.out.println();
+                    System.out.print("Do you want multiply(*), divide(/), add(+), or subtract(-) the percent: ");
+                    String operator2 = scanner.next();
+                    if (operator2.equals("+")) { // Addition option
+                        result = (num1 + ((num2 / 100) * num1));
+                        System.out.println("Result of addition with percent: " + result);
+                        System.out.println();
+                    } else if (operator2.equals("-")) { // Subtraction option
+                        result = (num1 - ((num2 / 100) * num1));
+                        System.out.println("Result of subtraction with percent: " + result);
+                        System.out.println();
+                    } else if (operator2.equals("*")) { // Multiplication option
+                        result = (num1 * (num2 / 100));
+                        System.out.println("Result of multiplication with percent: " + result);
+                        System.out.println();
+                    } else if (operator2.equals("/")) { // Division option
+                        result = (num1 / (num2 / 100));
+                        System.out.println("Result of division with percent: " + result);
+                        System.out.println();
+                    }
+                    break;
+                case "/": // Case for division
+                    if (num2 != 0) {
+                        System.out.println();
+                        result = num1 / num2;
+                        System.out.println("Result of division: " + result);
+                        System.out.println();
+                    } else { // Ensures cannot divide by zero
+                        System.out.println("Cannot divide by zero. Please try again.");
+                        System.out.println();
+                    }
+                    break;
+                case "*": // Case for multiplication
+                    System.out.println();
+                    result = num1 * num2;
+                    System.out.println("Result of multiplication: " + result);
+                    System.out.println();
+                    break;
+                case "+": // Case for addition
+                    System.out.println();
+                    result = num1 + num2;
+                    System.out.println("Result of addition: " + result);
+                    System.out.println();
+                    break;
+                case "-": // Case for subtraction
+                    System.out.println();
+                    result = num1 - num2;
+                    System.out.println("Result of subtraction: " + result);
+                    System.out.println();
+                    break;
+                case "SQRT": // Case for square root
+                    System.out.println();
+                    result = Math.sqrt(num1);
+                    System.out.println("Result of square root: " + result);
+                    System.out.println();
+                    break;
+                case "^": // Case for exponent
+                    System.out.println();
+                    result = Math.pow(num1, num2);
+                    System.out.println("Result of exponent: " + result);
+                    System.out.println();
+                    break;
+                case "Q": // Case for quit
+                    System.out.println();
+                    System.out.println("Thank you for using the calculator.");
+                    quit = true;
+                    break;
+            }
         }
+        while (!quit); // Do while loop quit
     }
 }
